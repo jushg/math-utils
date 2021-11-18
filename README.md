4b47e22846e6508984d28629a2e53626d177dba5
Tan <e0550425@u.nus.edu>
2021-11-18 16:51:33 +0800

    Add all distributions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # math-utils
 Collection of functions for math
+
+Adopted from [nvbinh15](https://github.com/nvbinh15)
---
+++ main-distribution.py
@@ -0,0 +1,11 @@
+from distributions.binomial import binomial
+from distributions.chi_square import chi_sq
+from distributions.exponential import exponential
+from distributions.f import f
+from distributions.negative_binomial import neg_binomial
+from distributions.normal import normal
+from distributions.poisson import poisson
+from distributions.t import t
+
+#test code
+b = binomial(1,2)
